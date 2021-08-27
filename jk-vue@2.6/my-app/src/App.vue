580468dfad268feb433c26eff9adf50ce770c97b
Jamboy <jamboylive@gmail.com>
2021-08-27 11:29:06 +0800

    init

---
+++ jk-vue@2.6/my-app/src/App.vue
@@ -0,0 +1,46 @@
+<!--
+ * @Description: 
+ * @Author: Jamboy
+ * @Date: 2021-08-27 10:35:28
+ * @LastEditTime: 2021-08-27 10:58:13
+-->
+<template>
+  <div id="app">
+    <input type="text" v-model="item" />
+    <button type="button" @click="handleAdd">添加</button>
+    <ul v-for="item in list" :key="item">
+      <TodoItem>
+        <template v-slot:item>
+          <span style="font-size: 20px">{{ item }}</span>
+        </template>
+      </TodoItem>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoItem
+  },
+
+  data() {
+    return {
+      item: '',
+      list: []
+    }
+  },
+  methods: {
+    handleAdd() {
+      this.list.push(this.item)
+      this.item = ''
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ jk-vue@2.6/my-app/src/components/TodoItem.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Description: 
+ * @Author: Jamboy
+ * @Date: 2021-08-27 10:39:33
+ * @LastEditTime: 2021-08-27 10:59:29
+-->
+<template>
+  <li>
+    <input type="checkbox" v-model="checked" />
+    <slot name="item"></slot>
+  </li>
+</template>
+<script>
+export default {
+  name: 'TodoItem',
+  props: ['info'],
+  data() {
+    return {
+      checked: true
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
